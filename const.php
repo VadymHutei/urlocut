8082c8e7eb35b4159fd12920ee984e11c6ba2d00
Vadym Hutei <hutei@live.com>
2020-09-25 00:02:55 +0300

    constants

---
+++ const.php
@@ -0,0 +1,7 @@
+<?php
+
+define('HOST_NAME', 'localhost:9000');
+define('ALIAS_LENGTH', 6);
+define('ALIAS_PATTERN', '/[a-z0-9]{6}/');
+define('LINK_PATTERN', '/[[:word:]]{3,256}/');
+define('DATETIME_FORMAT', 'Y-m-d G:i:s');
